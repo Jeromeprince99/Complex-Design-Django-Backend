0c214900d6e3582731dace33883d2ca7567bc3c6
Jeromeprince99 <jeromeprince99@gmail.com>
2020-06-18 19:08:21 +0530

    Commit

--- complex_design_backend/models.py
+++ complex_design_backend/models.py
@@ -4,6 +4,6 @@ from datetime import datetime
 class Post(models.Model):
     content = models.TextField()
     time = models.DateTimeField(blank=True, default=datetime.now )
-    image =  models.ImageField(upload_to="./images/", blank=True)
+    image =  models.ImageField(upload_to="images", blank=True)
     video = models.TextField(blank=True)
-    
+   
