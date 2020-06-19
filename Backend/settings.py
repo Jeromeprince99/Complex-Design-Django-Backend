447ea87dcde844b762dbbac515170d36a04e588b
Jeromeprince99 <jeromeprince99@gmail.com>
2020-06-19 20:28:32 +0530

    Commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -144,7 +144,4 @@ if os.path.isfile(dotenv_file):
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-# This should already be in your settings.py
 django_heroku.settings(locals())
-# This is new
-del DATABASES['default']['OPTIONS']['sslmode']
