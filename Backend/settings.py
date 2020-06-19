60863001039f8d3bcd68d052331941b04affd631
Jeromeprince99 <jeromeprince99@gmail.com>
2020-06-19 12:33:28 +0530

    Commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -147,4 +147,4 @@ USE_TZ = True
 # uncommented before pushing to production
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=True) }
